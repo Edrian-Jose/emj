2d563071055dd05e14ac1a0f7eedbf9cd86e23af
Edrian-Jose <edrianjoseferrer@gmail.com>
2022-03-14 14:46:11 +0800

    add force parameter  to getChannelWebhook

--- src/actions/Channel/Webhook/getChannelWebhook.ts
+++ src/actions/Channel/Webhook/getChannelWebhook.ts
@@ -4,19 +4,25 @@ import type { NewsChannel, Webhook } from 'discord.js';
 import syncChannel from '../syncChannel';
 import type { TextChannel } from 'discord.js';
 
-export const registerWebhook = async (channel: TextChannel | NewsChannel) => {
-	await channel.createWebhook('Emj Webhook', {
-		avatar: DefaultAvatar
+export const registerWebhook = async (channel: TextChannel | NewsChannel, name?: string, avatarURL?: string) => {
+	const webhook = await channel.createWebhook(name || 'Emj Webhook', {
+		avatar: avatarURL || DefaultAvatar
 	});
 
-	await syncChannel(channel.guild, channel);
+	const [_channel] = await syncChannel(channel.guild, channel);
+	if (_channel?.webhookId) {
+		return webhook;
+	}
+	return null;
 };
 
-const getChannelWebhook = async (channel: TextChannel | NewsChannel) => {
+const getChannelWebhook = async (channel: TextChannel | NewsChannel, force = false) => {
 	let webhook: Webhook | null = null;
 	const [_channel] = await getChannelDocument(channel.guild, channel);
 	if (_channel?.webhookId) {
 		webhook = await channel.client.fetchWebhook(_channel.webhookId);
+	} else if (force) {
+		webhook = await registerWebhook(channel);
 	}
 	return webhook;
 };
