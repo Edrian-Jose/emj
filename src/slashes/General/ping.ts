880ce537fdae1dc5f914fcbe277c063ef8818653
Edrian-Jose <edrianjoseferrer@gmail.com>
2022-04-13 00:50:56 +0800

    perform todos

--- src/slashes/General/ping.ts
+++ src/slashes/General/ping.ts
@@ -2,7 +2,7 @@ import { container } from '@sapphire/framework';
 import { SlashCommandBuilder } from '@discordjs/builders';
 import type { CommandInteraction } from 'discord.js';
 
-export const data = new SlashCommandBuilder().setName('ping').setDescription('wala lang');
+export const data = new SlashCommandBuilder().setName('ping').setDescription('Check if bot is working');
 
 export const execute = async (interaction: CommandInteraction) => {
 	const content = `Pong! Bot Latency ${Math.round(container.client.ws.ping)}ms`;
