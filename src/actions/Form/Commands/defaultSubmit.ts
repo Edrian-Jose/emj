89562b324d838189611823d07f42a1ce58b3579d
Edrian-Jose <edrianjoseferrer@gmail.com>
2022-04-14 02:13:34 +0800

    defaultSubmit changed

--- src/actions/Form/Commands/defaultSubmit.ts
+++ src/actions/Form/Commands/defaultSubmit.ts
@@ -1,7 +1,32 @@
-import type { FormCommand } from '../handleFormCommand';
+import { userMention } from '@discordjs/builders';
+import answersForm from '../../../components/embeds/answersForm';
+import parseChannel from '../../Channel/parseChannel';
+import type FormEntry from '../../FormEntry/FormEntry';
+import { getGuildDocument } from '../../Guild/syncGuild';
+import parseMember from '../../Member/parseMember';
+import type { EntryAnswer } from '../handleFormCommand';
 
-const defaultSubmit: FormCommand = async (...answers) => {
-	console.log(answers);
+const defaultSubmit = async (entry: FormEntry, ...answers: EntryAnswer[]): Promise<void> => {
+	if (entry.location.guildId) {
+		const [_guild, guild] = await getGuildDocument(entry.location.guildId);
+		let [member] = await parseMember(entry.location.guildId, entry.ownerId);
+
+		if (_guild && _guild.channels.feeds && member) {
+			const [feedsChannel] = await parseChannel(guild, _guild.channels.feeds);
+			if (feedsChannel?.isText()) {
+				feedsChannel.send({
+					embeds: [
+						answersForm(
+							`${member.nickname ? member.nickname : member.user.username} response to ${entry.form.title}`,
+							`This information may be irrelevant and can contain errors. Report to the managers if you find one. `,
+							answers
+						)
+					],
+					content: `@everyone ${userMention(member.id)}`
+				});
+			}
+		}
+	}
 };
 
 export default defaultSubmit;
