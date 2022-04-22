774f3f20471fbb70423ac10c876751e0a22b9dbb
Edrian-Jose <edrianjoseferrer@gmail.com>
2022-04-22 11:34:06 +0800

    refactor delStudent to delete all records

--- src/actions/RStudent/Subactions/SubmitActions/delSubmit.ts
+++ src/actions/RStudent/Subactions/SubmitActions/delSubmit.ts
@@ -5,16 +5,17 @@ import type RStudent from '../../RStudent';
 import parseChannel from '../../../Channel/parseChannel';
 import moment from 'moment';
 import rencode from '../../../../lib/rencode';
+import type { Location } from '../../../../schemas/RStudent';
 
 const delStudent = async (rstudent: RStudent, reason: string) => {
-	const { status } = rstudent;
+	const { status, reference } = rstudent;
 
 	const logSheet = await getSpreadsheetDocument(rencode.sheet, rencode.tabs.log);
-	const tab = status === 'trainee' ? rencode.tabs.trainee : rencode.tabs.student;
+	const tab = rstudent.removedAt ? rencode.tabs.out : rstudent.status === 'trainee' ? rencode.tabs.trainee : rencode.tabs.student;
 	const sheet = await getSpreadsheetDocument(rencode.sheet, tab);
 	const deletedSheet = await getSpreadsheetDocument(rencode.sheet, rencode.tabs.deleted);
-	const logRow = await findRow(logSheet, rstudent.reference);
-	const statusRow = await findRow(sheet, rstudent.reference);
+	const logRow = await findRow(logSheet, reference);
+	const statusRow = await findRow(sheet, reference);
 	if (logRow) {
 		const row = await deletedSheet.addRow([...logRow._rawData, reason]);
 		row['TIMESTAMP'] = moment().utcOffset(8).format('MM/DD/YYYY hh:mm A');
@@ -25,18 +26,23 @@ const delStudent = async (rstudent: RStudent, reason: string) => {
 	if (statusRow) {
 		await statusRow.delete();
 	}
-	if (rstudent.locations) {
-		if (rstudent.locations.log) {
-			const [allChannel] = await parseChannel(rstudent.locations.log.guildId, rstudent.locations.log.channelId);
-			if (allChannel?.isText()) {
-				await allChannel.messages.delete(rstudent.locations.log.messageId);
-			}
+
+	if (status === 'trainee') {
+		const infoSheet = await getSpreadsheetDocument(rencode.sheet, rencode.tabs.information);
+		const infoRow = await findRow(infoSheet, reference);
+		if (infoRow) {
+			await infoRow.delete();
 		}
-		const location = status === 'trainee' ? rstudent.locations.trainee : rstudent.locations.student;
-		if (location) {
-			const [channel] = await parseChannel(location.guildId, location.channelId);
-			if (channel?.isText()) {
-				await channel.messages.delete(location.messageId);
+	}
+
+	if (rstudent.locations) {
+		for (const type in rstudent.locations) {
+			if (Object.prototype.hasOwnProperty.call(rstudent.locations, type)) {
+				const location: Location = rstudent.locations[type];
+				const [channel] = await parseChannel(location.guildId, location.channelId);
+				if (channel?.isText()) {
+					await channel.messages.delete(location.messageId);
+				}
 			}
 		}
 	}
