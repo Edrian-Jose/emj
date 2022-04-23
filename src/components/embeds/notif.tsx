29c118a87daf27ab85f89a041144dd841286683e
Edrian-Jose <edrianjoseferrer@gmail.com>
2022-04-23 10:49:20 +0800

    change to only subtract 1 day instead of subtracting 1 week

--- src/components/embeds/notif.tsx
+++ src/components/embeds/notif.tsx
@@ -1,11 +1,12 @@
 import { EmbedJsx } from '@sapphire/embed-jsx';
+import moment from 'moment';
 import { DefaultFooter } from '../../lib/constants';
 import randomColor from '../../lib/randomColor';
 
 function RNotif(forScreening: string[], forDropping: string[]) {
 	return (
 		<embed color={randomColor()}>
-			<title>R-Encode Weekly Notification</title>
+			<title>{`R-Encode Week ${moment().subtract(1, 'day').week()} Notification`}</title>
 			<description>Weekly notifications that needs attention</description>
 			<field name="For Screening">{forScreening.length ? forScreening.join(',\n') : '_No students_'}</field>
 			<field name="For Dropping">{forDropping.length ? forDropping.join(',\n') : '_No students_'}</field>
